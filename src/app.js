46658c5320264fc1264cbf1987d1d5a963580640
Ron Olivar <rolivar@dnamicro.com>
2016-06-15 13:59:49 +0800

    Finished until testing configuration. Now on to 'our testing strategy' topic.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = React.createClass({
-  render: function() {
-    return (<div>Text text text</div>);
-  }
-});
+import './app.css'
+
+import 'font-awesome/css/font-awesome.css'
+
+import App from 'containers/App/App'
 
 const mountNode = document.querySelector('#root');
 ReactDOM.render(<App />,mountNode);
--- webpack.config.js
+++ webpack.config.js
@@ -14,11 +14,10 @@ const dest    = join(root, 'dist');
 
 const NODE_ENV = process.env.NODE_ENV;
 const isDev = NODE_ENV === 'development';
+const isTest = NODE_ENV === 'test';
 // alternatively, we can use process.argv[1]
 // const isDev = (process.argv[1] || '')
 //                .indexOf('hjs-dev-server') !== -1;
-
-// ...
 
 var config = getConfig({
   isDev: isDev,
@@ -26,5 +25,98 @@ var config = getConfig({
   out: dest,
   clearBeforeBuild: true
 })
+
+config.resolve.root = [src,modules]
+config.resolve.alias = {
+  'css': join(src, 'styles'),
+  'containers': join(src, 'containers'),
+  'components': join(src, 'components'),
+  'utils': join(src, 'utils')  
+}
+
+
+if (isTest) {
+  config.externals = {
+    'react/lib/ReactContext': true,
+    'react/lib/ExecutionEnvironment': true,
+    'react/addons': true
+  }
+
+  config.plugins = config.plugins.filter(p => {
+    const name = p.constructor.toString();
+    const fnName = name.match(/^function (.*)\((.*\))/)
+
+    const idx = [
+      'DedupePlugin',
+      'UglifyJsPlugin'
+    ].indexOf(fnName[1]);
+    return idx < 0;
+  })    
+}
+
+const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
+
+const matchCssLoaders = /(^|!)(css-loader)($|!)/;
+
+const findLoader = (loaders, match) => {
+  const found = loaders.filter(l => l &&
+      l.loader && l.loader.match(match));
+  return found ? found[0] : null;
+}
+
+config.postcss = [].concat([
+  require('precss')({}),
+  require('autoprefixer')({}),
+  require('cssnano')({})
+])
+
+// existing css loader
+const cssloader =
+  findLoader(config.module.loaders, matchCssLoaders);
+
+const newloader = Object.assign({}, cssloader, {
+  test: /\.module\.css$/,
+  include: [src],
+  loader: cssloader.loader
+    .replace(matchCssLoaders,
+    `$1$2?modules&localIdentName=${cssModulesNames}$3`)
+})
+
+config.module.loaders.push(newloader);
+cssloader.test =
+  new RegExp(`[^module]${cssloader.test.source}`)
+cssloader.loader = newloader.loader
+
+config.module.loaders.push({
+  test: /\.css$/,
+  include: [modules],
+  loader: 'style!css'
+})
+
+const dotenv = require('dotenv');
+const dotEnvVars = dotenv.config();
+
+const environmentEnv = dotenv.config({
+  path: join(root, 'config', `${NODE_ENV}.config.js`),
+  silent: true,
+});
+
+const envVariables =
+    Object.assign({}, dotEnvVars, environmentEnv);
+
+const defines =
+  Object.keys(envVariables)
+  .reduce((memo, key) => {
+    const val = JSON.stringify(envVariables[key]);
+    memo[`__${key.toUpperCase()}__`] = val;
+    return memo;
+  }, {
+    __NODE_ENV__: JSON.stringify(NODE_ENV)
+  });
+
+config.plugins = [
+  new webpack.DefinePlugin(defines)
+].concat(config.plugins);
+
 
 module.exports = config;
